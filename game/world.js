46a9e32d5218af9e5fb21abf6f1d4c4d196a076c
James Robinson <james.michael.robinson@googlemail.com>
2016-05-17 09:49:59 -0700

    Add world to server

---
+++ game/world.js
@@ -0,0 +1,6 @@
+var world = {
+  WIDTH: 500,
+  HEIGHT: 340
+};
+
+module.exports = world;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var world = require('./game/world');
 
 var PORT = 3001;
 
