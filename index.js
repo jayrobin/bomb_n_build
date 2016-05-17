ebc6be6fd0af288cc8ae64c38aa34cd765a0c164
James Robinson <james.michael.robinson@googlemail.com>
2016-05-16 20:29:35 -0700

    Move index.html to public directory

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 
 var PORT = 3002;
+var PUBLIC_DIR = __dirname + '/public';
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(PUBLIC_DIR + '/index.html');
 })
 
 http.listen(PORT, function() {
