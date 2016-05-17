09f04563084b5e3efcef5ccde905b602d3134540
James Robinson <james.michael.robinson@googlemail.com>
2016-05-16 20:41:29 -0700

    Register socket connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var PORT = 3002;
 
@@ -10,6 +11,12 @@ app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
+io.on('connection', handleConnection);
+
 http.listen(PORT, function() {
   console.log('Server initialized on port: ' + PORT);
 });
+
+function handleConnection() {
+  console.log('User connected');
+}
