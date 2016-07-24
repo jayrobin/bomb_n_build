adc56dbf5dbe0dde5b0de7a0d828d891fe9ff0cc
James Robinson <james.michael.robinson@googlemail.com>
2016-07-24 11:47:37 -0700

    Add info text to menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,12 +1,15 @@
 var menuState = {
   create: function() {
-    var nameLabel = game.add.text(game.world.centerX, -50, 'Multi Game', { font: '50px Geo', fill: '#ffffff' });
+    var nameLabel = game.add.text(game.world.centerX, 50, 'Multi Game', { font: '50px Arial', fill: '#ffffff' });
     nameLabel.anchor.setTo(0.5, 0.5);
 
-    var startLabel = game.add.text(game.world.centerX, game.world.height - 80, 'Press the up arrow key to start', { font: '25px Arial', fill: '#ffffff' });
+    var infoLabel = game.add.text(game.world.centerX, game.world.height / 2, "SPACE to drop bombs\nSHIFT to build", { font: '20px Arial', fill: '#ffffff', align: 'center' });
+    infoLabel.anchor.setTo(0.5, 0.5);
+
+    var startLabel = game.add.text(game.world.centerX, game.world.height - 80, 'Press ENTER to start', { font: '25px Arial', fill: '#ffffff' });
     startLabel.anchor.setTo(0.5, 0.5);
 
-    var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    var upKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     upKey.onDown.addOnce(this.start, this);
   },
   start: function() {
