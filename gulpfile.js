693529e10f96c1a95c902b4e3cbdbf2a22190d09
James Robinson <james.michael.robinson@googlemail.com>
2016-07-28 22:18:09 -0700

    Install gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const util = require('gulp-util');
 const watch = require('gulp-watch');
+const clean = require('gulp-clean');
 
 gulp.task('js', () => {
   gulp.src('client/*.js')
       .pipe(uglify().on('error', util.log))
       .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('clean', () => {
+  return gulp.src('public/js', { read: false })
+             .pipe(clean());
 });
